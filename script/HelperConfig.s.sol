fef72effd969466c21016d0ce1dafbb2bd054bef
noz <nowfeel.safi@gmail.com>
2025-06-08 12:45:29 +0200

    ci: commit pre-build ci yml feat: mock

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -6,6 +6,7 @@
 pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "../test/mock/MockV3Aggregator.sol";
 
 contract HelperConfig is Script {
     NetworkConfig public activeNetworkConfig;
@@ -14,6 +15,10 @@ contract HelperConfig is Script {
         address priceFeed; // ETH/USD price feed address
     }
 
+    // When i deploy the contract, i check on which chain i am (in function of the chain_id) and i call the correct
+    // function
+    //  - if chain_id of eth seplia: i set the price feed of eth sepolia
+    //  - else: i set the anvil price feed
     constructor() {
         if (block.chainid == 11155111) {
             activeNetworkConfig = getSepoliaETHConfig();
@@ -22,13 +27,27 @@ contract HelperConfig is Script {
         }
     }
 
+    // I set the price feed on ETH sepolia chain
     function getSepoliaETHConfig() public pure returns (NetworkConfig memory) {
-        NetworkConfig sepoliaConfig = NetworkConfig({
+        NetworkConfig memory sepoliaConfig = NetworkConfig({
         priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
     });
 
         return sepoliaConfig;
     }
 
-    function getAnvilETHConfig() public pure returns (NetworkConfig memory) {}
+    /*
+        flora ma bella
+        boubou la poilu
+    */
+
+    function getAnvilETHConfig() public returns (NetworkConfig memory) {
+        vm.startBroadcast();
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(8, 2000e8);
+        vm.stopBroadcast();
+
+        NetworkConfig memory anvilConfig = NetworkConfig({priceFeed: address(mockPriceFeed)});
+
+        return anvilConfig;
+    }
 }
