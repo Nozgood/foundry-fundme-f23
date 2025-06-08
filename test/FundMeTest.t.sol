cfea78f90f90f372ff40c269458553edee364e75
noz <nowfeel.safi@gmail.com>
2025-06-08 21:08:19 +0200

    wip "

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -9,9 +9,14 @@ import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 contract FundMeTest is Test {
     FundMe fundMe;
 
+    address private user = makeAddr("test");
+    uint256 private constant SEND_VALUE = 0.1 ether;
+    uint256 private constant STARTING_BALANCE = 10 ether;
+
     function setUp() external {
         DeployFundMe deployFundMe = new DeployFundMe();
         fundMe = deployFundMe.run();
+        vm.deal(user, STARTING_BALANCE);
     }
 
     function testMinimumUSDIsFive() public view {
@@ -26,5 +31,17 @@ contract FundMeTest is Test {
         uint256 version = fundMe.getVersion();
 
         assertEq(version, 4);
+    }
+
+    function testFundFailsWithoutEnoughETH() public {
+        vm.expectRevert();
+        fundMe.fund();
+    }
+
+    function testFundUpdateFundedDataStructure() public {
+        vm.prank(user); // The next TX will be sent by user address
+        fundMe.fund{value: SEND_VALUE}();
+        uint256 amountFunded = fundMe.getAddressToAmountFunded(user);
+        assertEq(amountFunded, SEND_VALUE);
     }
 }
