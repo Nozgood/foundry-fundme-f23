df773fccf96c2d434c1b883dab85ddd3d994db40
nowfeel <nowfeel.safi@gmail.com>
2025-08-04 17:45:09 +0200

    test: unit test

---
+++ test/integration/InteractionTest.t.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {DeployFundMe} from "../../script/DeployFundMe.s.sol";
+import {FundMe} from "../../src/FundMe.sol";
+import {FundFundMe} from "../../script/Interactions.s.sol";
+
+contract FundMeTestIntegration is Test {
+    FundMe fundMe;
+
+    address private user = makeAddr("test");
+    uint256 private constant SEND_VALUE = 0.1 ether;
+    uint256 private constant STARTING_BALANCE = 10 ether;
+
+    function setUp() external {
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
+        vm.deal(user, STARTING_BALANCE);
+    }
+
+    function testUserCanFundInteractions() public {
+        FundFundMe fundFundMe = new FundFundMe();
+        vm.prank(user);
+        vm.deal(user, 1e18);
+
+        fundFundMe.fundFundMe(address(fundMe));
+
+        assertEq(fundMe.getFunder(0), address(user));
+    }
+}
